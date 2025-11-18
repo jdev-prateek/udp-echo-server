2101c0712c9f26baa8063188111b8242282822f0
Prateek <prateek.iq@gmail.com>
2025-11-18 08:31:01 +0530

    init

--- src/main/java/org/example/core/UDPServer.java
+++ src/main/java/org/example/core/UDPServer.java
@@ -1,10 +1,12 @@
-package org.example.server;
+package org.example.core;
 
+import org.example.util.Common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
 import java.util.Optional;
 
 public class UDPServer {
@@ -15,21 +17,57 @@ public class UDPServer {
     private static DatagramSocket socket;
 
 
-    private static void init(){
+    private static void init() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("\nUDP Server is shutting down...");
+            if (socket != null && !socket.isClosed()) {
+                socket.close();
+            }
+        }));
+
         try {
+            byte[] bytes = new byte[256];
+            DatagramPacket packet = new DatagramPacket(bytes, 256);
             socket = new DatagramSocket(PORT);
-        } catch (SocketException e) {
-            logger.error("Failed to start UDP server", e);
+
+            while (true) {
+                socket.receive(packet);
+                DatagramPacket datagramPacket = Common.copyDatagram(packet);
+
+                new Thread(() -> {
+                    try {
+                        handleRequest(datagramPacket);
+                    } catch (IOException e) {
+                        logger.error("Error handling request", e);
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
+            logger.error("UDP server error ", e);
         }
     }
 
-    public static int gerPort(){
+    private static void handleRequest(DatagramPacket packet) throws IOException {
+        logger.info("#".repeat(10));
+        logger.info("Received packet from " + packet.getAddress() + ":" + packet.getPort());
+        String message = new String(packet.getData(), 0, packet.getLength());
+        logger.info("Message: " + message.trim());
+
+        byte[] responseData = ("Echo: " + message).getBytes();
+        DatagramPacket datagramPacket = new DatagramPacket(responseData, responseData.length, packet.getAddress(),
+                packet.getPort());
+        socket.send(datagramPacket);
+        logger.info("Sent echo response to " + packet.getAddress() + ":" + packet.getPort());
+        logger.info("#".repeat(10));
+    }
+
+    public static int gerPort() {
         return Optional.ofNullable(System.getenv(ENV_UDP_ECHO_PORT))
                 .map(Integer::parseInt)
                 .orElse(DEFAULT_PORT);
     }
 
-    public static void start(){
+    public static void start() {
         logger.info("Starting UDP ECHO Server");
         init();
     }
--- src/main/java/org/example/util/Common.java
+++ src/main/java/org/example/util/Common.java
@@ -1,4 +1,17 @@
 package org.example.util;
 
+import java.net.DatagramPacket;
+
 public class Common {
+    public static DatagramPacket copyDatagram(DatagramPacket src){
+        int len = src.getLength();
+        byte[] dataCopy = new byte[len];
+        System.arraycopy(src.getData(), 0, dataCopy, 0, len);
+
+        DatagramPacket newDatagramPacket = new DatagramPacket(dataCopy, len);
+        newDatagramPacket.setPort(src.getPort());
+        newDatagramPacket.setAddress(src.getAddress());
+
+        return newDatagramPacket;
+    }
 }
